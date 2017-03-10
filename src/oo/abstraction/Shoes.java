7af9b8ac4bc7faffc701ea56473fd0c0879e97d2
Lin iris <iris861219@gmail.com>
2017-03-10 13:19:02 +0800

    Shoes

---
+++ src/oo/abstraction/Shoes.java
@@ -0,0 +1,17 @@
+package oo.abstraction;
+
+public class Shoes {
+	String brand;
+	float size;
+	String type;
+	String color;
+	String material;
+	public Shoes(String brand,float size,String type,String color,String material){
+		this.brand=brand;
+		this.size = size;
+		this.type = type;
+		this.color=color;
+		this.material=material;
+	}
+	
+}
---
+++ src/oo/abstraction/ShoesTester.java
@@ -0,0 +1,9 @@
+package oo.abstraction;
+
+public class ShoesTester {
+
+	public static void main(String[] args) {
+		Shoes s1 = new Shoes("Mizuno",24.5f,"jogging","white"," ");
+	}
+
+}
