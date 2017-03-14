7bbc36e478ea37da3bf2dafcd2eed1559c0d2b71
Iris Lin <iris861219@gmail.com>
2017-03-14 15:56:08 +0800

    新增建構子

--- src/oo/abstraction/CarTester.java
+++ src/oo/abstraction/CarTester.java
@@ -3,6 +3,8 @@ package oo.abstraction;
 public class CarTester {
 
 	public static void main(String[] args) {
-		Car c1 = new Car("Toyota",1800,"Altis","Sedan");
+		Car c1 = new Car("Toyota", 1800, "Altis", "Sedan");
+		Car c2 = new Car("HONDA", 2000, "ACCORD", "Sedan");
+		Car c3 = new Car("Toyota", 2000, "WISH", "Sedan");
 	}
 }
