36761896ad6cb1a87190980ca7eb2e07031a35ca
Iris Lin <iris861219@gmail.com>
2017-03-23 15:24:38 +0800

    Book

--- src/oo/abstraction/Book.java
+++ src/oo/abstraction/Book.java
@@ -5,6 +5,7 @@ public class Book {
 	String language;
 	int version;
 	String title;
+	String author;
 	public Book(String type,String language,int version,String title){
 		this.type=type;
 		this.language=language;
--- src/oo/abstraction/BookTester.java
+++ src/oo/abstraction/BookTester.java
@@ -4,6 +4,25 @@ public class BookTester {
 
 	public static void main(String[] args) {
 		Book b1 = new Book("psychology", "繁體中文",1,"被討厭的勇氣");
+		Book b2 = new Book("psychology", "繁體中文",1,"情緒勒索");
+		Book b3 = new Book("poetry", "繁體中文",1,"女演員");
+		
+		Book[] books = new Book[3];
+		books[0] = new Book("psychology", "繁體中文",1,"被討厭的勇氣");
+		books[1] = new Book("psychology", "繁體中文",1,"情緒勒索");
+		books[2] = new Book("poetry", "繁體中文",1,"女演員");
+		books[0].author = " 岸見一郎";
+		books[1].author =" 周慕姿";
+		books[2].author = "連俞涵";
+		
+		int i;
+		for(i=0;i<3;i++){
+			System.out.println(books[i].title);
+			System.out.println("作者: "+books[i].author);
+			
+		}
+		System.out.println(i);
+		
 	}
 
 }
