a0e935f8cd37204f02df5d272ba571951cb58c1f
Lin iris <iris86@gmail.com>
2017-03-30 13:29:16 +0800

    invoice

--- src/oo/test/Invoice.java
+++ src/oo/test/Invoice.java
@@ -11,17 +11,26 @@ public class Invoice {
 		a.add("821");
 		a.add("886");
 		a.add("554");
-
+		int c = 0;
+		
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("請輸入發票號碼: ");
 		String s = scanner.nextLine();
-		String nu = s.substring(s.length()-3);
-		//String st = Integer.toString(n);
+		String nu = s.substring(s.length() - 3);
+		// String st = Integer.toString(n);
 		for (int i = 0; i < 4; i++) {
-			if (nu == a.get(i)) {
+			if (nu.equals(a.get(i))) {
 				System.out.println("中獎");
+				break;
+			} else {
+				c = c + 1;
 			}
-
+			if (c == 4) {
+				System.out.println("沒中獎");
+			}
+//			System.out.println(a.get(i));
+//			System.out.println(nu);
+//			System.out.println(c);
 		}
 
 	}
